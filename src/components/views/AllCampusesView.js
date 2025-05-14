995ce295c139a5445ef532fe095d9f91638e6c46
Cruz <serinmathew000@gmail.com>
2025-05-14 19:26:37 -0400

    Added all campus view

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -8,37 +8,67 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const AllCampusesView = (props) => {
-  // If there is no campus, display a message.
-  if (!props.allCampuses.length) {
-    return <div>There are no campuses.</div>;
+  const { allCampuses } = props;
+
+  if (!allCampuses.length) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "40px" }}>
+        <h2>There are no campuses.</h2>
+        <Link to="/">
+          <button>Add New Campus</button>
+        </Link>
+      </div>
+    );
   }
 
-  // If there is at least one campus, render All Campuses view 
   return (
-    <div>
-      <h1>All Campuses</h1>
+    <div style={{ backgroundColor: "#dbeaf1", padding: "30px" }}>
+      <h1 style={{ color: "#1c2372", textAlign: "center" }}>All Campuses</h1>
 
-      {props.allCampuses.map((campus) => (
-        <div key={campus.id}>
-          <Link to={`/campus/${campus.id}`}>
-            <h2>{campus.name}</h2>
-          </Link>
-          <h4>campus id: {campus.id}</h4>
-          <p>{campus.address}</p>
-          <p>{campus.description}</p>
-          <hr/>
+      {allCampuses.map((campus) => (
+        <div
+          key={campus.id}
+          style={{
+            backgroundColor: "white",
+            padding: "20px",
+            margin: "20px auto",
+            borderRadius: "10px",
+            width: "500px",
+            textAlign: "center",
+            boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <h2>
+            <Link to={`/campus/${campus.id}`} style={{ color: "#2e087b", textDecoration: "underline" }}>
+              {campus.name}
+            </Link>
+          </h2>
+
+          <h4>Campus ID: {campus.id}</h4>
+
+          {campus.imageUrl && (
+            <img
+              src={campus.imageUrl}
+              alt="campus"
+              width="300"
+              height="180"
+              style={{ borderRadius: "6px", margin: "10px 0" }}
+            />
+          )}
+
+          <hr />
         </div>
       ))}
-      <br/>
-      <Link to={`/`}>
-        <button>Add New Campus</button>
-      </Link>
-      <br/><br/>
+
+      <div style={{ textAlign: "center", marginTop: "30px" }}>
+        <Link to="/">
+          <button>Add New Campus</button>
+        </Link>
+      </div>
     </div>
   );
 };
 
-// Validate data type of the props passed to component.
 AllCampusesView.propTypes = {
   allCampuses: PropTypes.array.isRequired,
 };
